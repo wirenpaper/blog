28624aa85b3cb202617ee376223caafb8209ccca
wnp <chumchumchira@gmail.com>
2025-03-29 16:51:13 +0500

    deletePostById tests created

---
+++ src/db/post/__tests__/post_repository_tests/delete_post_by_id.test.ts
@@ -0,0 +1,53 @@
+import sqlClient from "@src/db.js"
+import { postRepository } from "@db/post/post_repository.js"
+
+jest.mock("@src/db.js")
+
+describe("postRepository", () => {
+  describe("DeletePostById", () => {
+    beforeEach(() => {
+      jest.clearAllMocks()
+    })
+
+    it("Success", async () => {
+      (sqlClient as unknown as jest.Mock).mockResolvedValue(undefined)
+
+      // Act
+      const result = await postRepository(sqlClient).deletePostById({
+        id: 123,
+      })
+
+      // Assert
+      expect(result).toBe(undefined)
+    })
+
+    it("!e.code case", async () => {
+      const error = new Error("oops");
+      (sqlClient as unknown as jest.Mock).mockRejectedValue(error)
+
+      // Act & Assert
+      await expect(postRepository(sqlClient).deletePostById({
+        id: 123,
+      })).rejects.toMatchObject({
+        statusCode: 500,
+        message: "STATUSCODE NOT FOUND oops"
+      })
+    })
+
+    it("Postgres error", async () => {
+      (sqlClient as unknown as jest.Mock).mockRejectedValue({
+        code: "23505",  // This is a Postgres error code, like unique_violation
+        message: "duplicate key value violates unique constraint"
+      })
+
+      // Act & Assert
+      await expect(postRepository(sqlClient).deletePostById({
+        id: 123,
+      })).rejects.toMatchObject({
+        statusCode: 400,
+        message: "duplicate key value violates unique constraint"
+      })
+    })
+
+  })
+})
