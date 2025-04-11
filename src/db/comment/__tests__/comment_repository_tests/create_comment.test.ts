2d62ca13be0335553bc90b3d42b70a138206ee37
wnp <chumchumchira@gmail.com>
2025-04-11 11:16:33 +0500

    create_comment_test.ts added

---
+++ src/db/comment/__tests__/comment_repository_tests/create_comment.test.ts
@@ -0,0 +1,59 @@
+import sqlClient from "@src/db.js"
+import { commentRepository } from "@db/comment/comment_repository.js"
+
+jest.mock("@src/db.js")
+
+describe("commentRepository", () => {
+  describe("createComment", () => {
+    beforeEach(() => {
+      jest.clearAllMocks()
+    })
+
+    it("Success", async () => {
+      (sqlClient as unknown as jest.Mock<Promise<void>, []>).mockResolvedValue(undefined)
+
+      // Act
+      const result = await commentRepository(sqlClient).createComment({
+        mComment: "lol",
+        postId: 132,
+        userId: 22
+      })
+
+      // Assert
+      expect(result).toBe(undefined)
+    })
+
+    it("!e.code case", async () => {
+      const error = new Error("oops");
+      (sqlClient as unknown as jest.Mock).mockRejectedValue(error)
+
+      // Act & Assert
+      await expect(commentRepository(sqlClient).createComment({
+        mComment: "lol",
+        postId: 132,
+        userId: 22
+      })).rejects.toMatchObject({
+        statusCode: 500,
+        message: "STATUSCODE NOT FOUND oops"
+      })
+    })
+
+    it("Postgres error", async () => {
+      (sqlClient as unknown as jest.Mock).mockRejectedValue({
+        code: "23505",  // This is a Postgres error code, like unique_violation
+        message: "duplicate key value violates unique constraint"
+      })
+
+      // Act & Assert
+      await expect(commentRepository(sqlClient).createComment({
+        mComment: "lol",
+        postId: 132,
+        userId: 22
+      })).rejects.toMatchObject({
+        statusCode: 400,
+        message: "duplicate key value violates unique constraint"
+      })
+    })
+
+  })
+})
